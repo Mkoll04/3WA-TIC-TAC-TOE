93acd49144321e86a038bd374916c9ae21e45abf
Mkoll04 <marius.moura75@gmail.com>
2022-12-19 14:38:28 +0000

    maj projet morpion

--- script.js
+++ script.js
@@ -1,53 +1,74 @@
-let case1 = null
-// let case2 = document.getElementsByClassName("box")[1]
-// let case3 = document.getElementsByClassName("box")[2]
-// let case4 = document.getElementsByClassName("box")[3]
-// let case5 = document.getElementsByClassName("box")[4]
-// let case6 = document.getElementsByClassName("box")[5]
-// let case7 = document.getElementsByClassName("box")[6]
-// let case8 = document.getElementsByClassName("box")[7]
-// let case9 = document.getElementsByClassName("box")[8]
+let cell = null
+
+const jeu = ['','','','','','','','','']
+
 let tours = 0
-// let joueurs = ["X","O"]
-// tours =  tours % 2
-// console.log(case4)
-// let ticTac = [case1, case2, case3, case4, case5, case6, case7, case8, case9]
+
 document.addEventListener("DOMContentLoaded", () => {
-    case1 = document.getElementsByClassName("box")
+    cell = Array.from(document.getElementsByClassName('box'));
+    let winOrNo = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6],
+        [0, 4, 8]
+    ]
 
 
-    case1 = Array.from(document.getElementsByClassName('box'));
+    function winOrNot() {
+        winOrNo.forEach(casDeVictoire => {
+            const indexCell1 = casDeVictoire[0]
+            const indexCell2 = casDeVictoire[1]
+            const indexCell3 = casDeVictoire[2]
+            if(jeu[indexCell1] !== "" && jeu[indexCell2] !== "" && jeu[indexCell3] !== ""){
+                if(jeu[indexCell1] === jeu[indexCell2] && jeu[indexCell2] === jeu[indexCell3]){
+                    setTimeout(() => alert(`le gagnant est : ${jeu[indexCell1]}`), 200)
+                }
+            }
+        })
+    }
 
 
-
-    // console.log(case1[1].value)
 
     let click = (element) => {
+        const index = element.getAttribute("id")
         let joueurs = ["X", "O"]
         tours = tours % 2
 
         if (element.innerText === "") {
             element.innerText = joueurs[tours]
+            jeu[index] = joueurs[tours]
+            winOrNot()
             tours++
-            } else if (element[0].innerText === joueurs[tours] && element[1].innerText === joueurs[tours]&& element[2].innerText ===joueurs[tours]){
-           alert(joueurs[tours] + " win")
-}
-              
-      
+        }
         
-}
+    }
+    //             else if (element[0].innerText === joueurs[tours] && element[1].innerText === joueurs[tours]&& element[2].innerText ===joueurs[tours]){
+    //           alert(joueurs[tours] + " win")
+    // }
 
-    
-    case1.forEach(element => element.addEventListener("click", () => click(element)))
-    
+
+
+
+
+
+    cell.forEach(element => element.addEventListener("click", () => click(element)))
+
+    console.log(cell[1].innerText)
+
 
     let reset = document.getElementsByClassName("reset")[0]
     let reset1 = (element) => {
         element.innerText = ""
     }
     reset.addEventListener("click", () => {
-        case1.forEach(element => reset1(element))
-
+        cell.forEach(element => reset1(element))
+        for(let i=0; i>jeu.length;i++){
+            jeu[i]=""
+        }
 
         //   console.log(case1[1])  
 
