aaecd1970f62a7b2aa5e6cf03a80d2368a80b2c3
Mkoll04 <marius.moura75@gmail.com>
2022-12-19 09:44:20 +0000

    rajout fichiers

---
+++ script.js
@@ -0,0 +1,42 @@
+let case1 = document.getElementsByClassName("box")[0]
+let case2 = document.getElementsByClassName("box")[1]
+let case3 = document.getElementsByClassName("box")[2]
+let case4 = document.getElementsByClassName("box")[3]
+let case5 = document.getElementsByClassName("box")[4]
+let case6 = document.getElementsByClassName("box")[5]
+let case7 = document.getElementsByClassName("box")[6]
+let case8 = document.getElementsByClassName("box")[7]
+let case9 = document.getElementsByClassName("box")[8]
+
+let ticTac = [case1,case2,case3,case4,case5,case6,case7,case8,case9]
+document.addEventListener("DOMContentLoaded", () => {
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+})
