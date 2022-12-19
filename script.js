bd9b05f3408a6b8f43418e62b2ed1b26a2c4f72f
Mkoll04 <marius.moura75@gmail.com>
2022-12-19 11:30:38 +0000

    rajout fichiers

--- script.js
+++ script.js
@@ -1,31 +1,51 @@
-let case1 = document.getElementsByClassName("box")[0]
-let case2 = document.getElementsByClassName("box")[1]
-let case3 = document.getElementsByClassName("box")[2]
-let case4 = document.getElementsByClassName("box")[3]
-let case5 = document.getElementsByClassName("box")[4]
-let case6 = document.getElementsByClassName("box")[5]
-let case7 = document.getElementsByClassName("box")[6]
-let case8 = document.getElementsByClassName("box")[7]
-let case9 = document.getElementsByClassName("box")[8]
-// let tours = 0
+let case1 = null
+// let case2 = document.getElementsByClassName("box")[1]
+// let case3 = document.getElementsByClassName("box")[2]
+// let case4 = document.getElementsByClassName("box")[3]
+// let case5 = document.getElementsByClassName("box")[4]
+// let case6 = document.getElementsByClassName("box")[5]
+// let case7 = document.getElementsByClassName("box")[6]
+// let case8 = document.getElementsByClassName("box")[7]
+// let case9 = document.getElementsByClassName("box")[8]
+let tours = 0
 // let joueurs = ["X","O"]
 // tours =  tours % 2
-console.log(case4)
-
-let ticTac = [case1, case2, case3, case4, case5, case6, case7, case8, case9]
+// console.log(case4)
+// let ticTac = [case1, case2, case3, case4, case5, case6, case7, case8, case9]
 document.addEventListener("DOMContentLoaded", () => {
-    let click = (elements) => {
-        let tours = 0
+    case1 = document.getElementsByClassName("box")
+
+
+    case1 = Array.from(document.getElementsByClassName('box'));
+
+
+
+    console.log(case1[1].value)
+
+    let click = (element) => {
         let joueurs = ["X", "O"]
         tours = tours % 2
-        if (elements === undefined) {
-            for (let i = 0; i < 9; i++) {
-                elements.innerText = joueurs[tours]
-                tours++
-            }
+
+        if (element.innerText === "") {
+            element.innerText = joueurs[tours]
+            tours++
         }
     }
-    ticTac.forEach(element => element.addEventListener("click",() => click(element)))
+
+
+    case1.forEach(element => element.addEventListener("click", () => click(element)))
+    
+    let reset = document.getElementsByClassName("reset")[0]
+    let reset1 = (element) => {
+        element.innerText=""
+    } 
+    reset.addEventListener("click", () => {
+        case1.forEach(element => reset1(element))
+        
+        
+      console.log(case1[1])  
+        
+    })
 
 
 
